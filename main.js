44899e179be002c12a3c1ad9b2f61ea7f69f1306
CatalinGheorghiu <ionutcatalingheorghiu@yahoo.com>
2020-01-29 18:56:48 +0100

    testing

--- main.js
+++ main.js
@@ -1,42 +1,54 @@
 //Initialize and add the map
 
 function initMap() {
-    //Your Location
-    const loc = { lat: 45.748871, lng: 21.208679 };
-    //Centered map on location
-    const map = new google.maps.Map(document.querySelector(".map"), {
-        zoom: 12,
-        center: loc
-    });
-    //The marker, positioned at location
-    const marker = new google.maps.Marker({ position: loc, map: map });
+	//Your Location
+	const loc = { lat: 45.748871, lng: 21.208679 };
+	//Centered map on location
+	const map = new google.maps.Map(document.querySelector(".map"), {
+		zoom: 12,
+		center: loc
+	});
+	//The marker, positioned at location
+	const marker = new google.maps.Marker({ position: loc, map: map });
 }
 
 //Sticky menu background
 window.addEventListener("scroll", function() {
-    if (window.scrollY > 150) {
-        document.querySelector("#navbar").style.opacity = 0.9;
-    } else {
-        document.querySelector("#navbar").style.opacity = 1;
-    }
+	if (window.scrollY > 150) {
+		document.querySelector("#navbar").style.opacity = 0.9;
+	} else {
+		document.querySelector("#navbar").style.opacity = 1;
+	}
 });
 
 // Smooth Scroll
 
 $("#navbar a, .btn").on("click", function(event) {
-    if (this.hash !== "") {
-        // event.preventDefault();
-        // event.stopPropagation();
+	if (this.hash !== "") {
+		// event.preventDefault();
+		// event.stopPropagation();
 
-        const hash = this.hash;
-        // Using jQuery's animate() method to add smooth page scroll
-        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-        $("html, body").animate(
-            {
-                scrollTop: $(hash).offset().top - 100
-            },
-            800
-        );
-        return false;
-    }
+		const hash = this.hash;
+		// Using jQuery's animate() method to add smooth page scroll
+		// The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+		$("html, body").animate(
+			{
+				scrollTop: $(hash).offset().top - 100
+			},
+			800
+		);
+		return false;
+	}
 });
+
+$(".port-item").click(function () {
+    $('.collapse').collapse('hide');
+})
+
+$(document).on("click", "[data-toggle='lightbox']", function (e) {
+    e.preventDefault();
+    $(this).ekkoLightbox();
+})
+
+// Get the current year for the copyright
+$("#year").text(new Date().getFullYear());
